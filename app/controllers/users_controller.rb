21f6c003c3765c9fd6e07c281a45026a7c4465fc
keisuke <noooocchi30@gmail.com>
2016-11-03 03:11:00 +0000

    lesson7 kadai1_1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,13 +1,15 @@
 class UsersController < ApplicationController
 
-  def show # 追加
+before_action :set_user, only: [:edit, :update]
+
+  def show
    @user = User.find(params[:id])
+   
   end
   
   def new
     @user = User.new
   end
-  
   
   def create
     @user = User.new(user_params)
@@ -19,6 +21,10 @@ class UsersController < ApplicationController
     end
   end
   
+  def edit #追加
+
+  end
+  
   private
 
   def user_params
@@ -26,6 +32,11 @@ class UsersController < ApplicationController
                                  :password_confirmation)
   end
   
+ private
   
+ def set_user
+    @user = User.find(params[:id])
+ end
+ 
   
 end
