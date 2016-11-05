64416fdec7d1cc41e43374dc94bd938dca1e20f6
keisuke <noooocchi30@gmail.com>
2016-11-05 07:42:09 +0000

    kadai1 teisyutsu 1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,10 +1,8 @@
 class UsersController < ApplicationController
-before_action :set_user, only: [:edit, :update]
-
+before_action :current_user_check,only: [:edit,:update]
 
   def show
    @user = User.find(params[:id])
-   
   end
   
   def new
@@ -21,37 +19,34 @@ before_action :set_user, only: [:edit, :update]
     end
   end
   
-  def edit #追加
-
+  def edit
+   @user = User.find(params[:id])
   end
   
    def update
     if @user.update(user_params)
       # 保存に成功した場合はトップページへリダイレクト
-      redirect_to root_path , notice: 'メッセージを編集しました'
+      redirect_to root_path , notice: 'プロフィールを編集しました'
     else
       # 保存に失敗した場合は編集画面へ戻す
       render 'edit'
     end
    end
-  
-  
-  
-  
+
   private
 
   def user_params
     params.require(:user).permit(:name, :email, :password,
-                                 :password_confirmation)
+                                 :password_confirmation,:profile,:region)
   end
-  
   
   private
-
-  def set_user
+  
+  def current_user_check
     @user = User.find(params[:id])
+
+    if current_user != @user
+      redirect_to root_path , notice: 'ユーザーが異なります'
+    end
   end
-  
-  
-  
 end
